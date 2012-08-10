fe24f0b8a76ee51cade677a19478d4787a19978e
Francois Laptop <webarbeit@gmail.com>
2012-08-10 17:06:54 +0200

    Refactored. Added item list feature. Improved example with comments

--- sliderdiv.js
+++ sliderdiv.js
@@ -1,27 +1,42 @@
 /*
-  SliderDiv --v.1.2.0
+  SliderDiv --v.1.2.1
   Author: weberdevelopment.de
   https://github.com/webarbeit/SliderDiv
 */
 var SliderDiv = function(_options) {
+
    var defaults = {
-    containerSelector: '#slide-container',
-    slideWrapperSelector: '.slide-wrapper',
-    slideSelector: '.slide',
-    nextButtonSelector: '.nextButton',
-    previousButtonSelector: '.prevButton',
-    hasKeyEvents: false,
-    moveSpeed: 500
+
+    containerSelector : '#slide-container',
+
+    slideWrapperSelector : '.slide-wrapper',
+
+    slideSelector : '.slide',
+
+    nextButtonSelector : '.nextButton',
+  
+    previousButtonSelector : '.prevButton',
+
+    itemListSelector : null,
+
+    hasKeyEvents : false,
+
+    moveSpeed : 500
   }
+  
+  // Merge default settings with arguments
   this.settings = $.extend({}, defaults, _options);
 
   this.container = $(this.settings.containerSelector);
+  
   this.viewport = this.container.find(this.settings.slideWrapperSelector);
-  this.slideObj = this.viewport.find(this.settings.slideSelector);
+  this.slideObjects = this.viewport.find(this.settings.slideSelector);
+
+  this.itemUl = $(this.settings.itemListSelector);
+
   this.nextButton = this.container.find(this.settings.nextButtonSelector);
   this.prevButton = this.container.find(this.settings.previousButtonSelector);
 
-  this.currentSlide = null,
   this.MOVE_SPEED = this.settings.moveSpeed;
   this.HAS_KEY_EVENTS = this.settings.hasKeyEvents;
   
@@ -29,60 +44,140 @@ var SliderDiv = function(_options) {
 };
 
 SliderDiv.prototype.init = function() {
-    this.currentSlide = 0;
-    this.handleButtonVisibility();
-    this.autoTimeout = null;
-    this.doPlay = false;
-    this.autoMoveTime = 5000;
 
-    // Adapt size
-    var vp = this.container.width() * this.slideObj.length;
-    this.viewport.css({ width:  vp + 'px', overflow: 'overlay', clear: 'both'});
-    this.slideObj.css({ width: this.container.width() + 'px' });
+  this.reset();
 
-    // Event
-    this.nextButton.click($.proxy(this.next, this));
-    this.prevButton.click($.proxy(this.prev, this));
+  // Set Styles
+  this.container.css({ "overflow" : "hidden" });   
+  // Adapt size
+  var vp = this.container.width() * this.slideObjects.length;
+  
+  this.viewport.css({ 
+
+    width :  vp + 'px', 
+    overflow : 'overlay', 
+    clear : 'both',
+    position : "relative"
+
+  });
+  
+  this.slideObjects.css({ 
+
+    width: this.container.width() + 'px',
+    "position" : "relative",
+    "float" : "left"
+
+  });
+
+  this.buildItems();
+
+  // Events
+  this.nextButton.click($.proxy(this.next, this));
+  this.prevButton.click($.proxy(this.prev, this));
+
+  if(this.HAS_KEY_EVENTS)
     $(document).keydown($.proxy(this.keyEvent, this));
+  
+  return this;
+  
+};
 
-    this.countSlides = this.slideObj.length;
-    return this;
+SliderDiv.prototype.reset = function() {
+
+  this.currentSlide = 0;
+  this.handleButtonVisibility();
+  this.autoTimeout = null;
+  this.doPlay = false;
+  this.autoMoveTime = 5000;
+
+  this.countSlides = this.slideObjects.length;
+
+  return this;
+
+};
+
+SliderDiv.prototype.buildItems = function() {
+
+  var self = this;
+
+  if( !this.itemUl.length || this.countSlides === 0 ) return false;
+
+  this.slideObjects.each(function(k, v) {
+
+    self.itemUl.append('<li class="slide-item"></li>');
+
+  });
+
+  // Bind event
+  this.itemUl.find("li").bind("click", function() {
+
+    self.moveTo( $(this).index() );
+
+  });
+
+  this.highLightItem();
+
 };
 
 SliderDiv.prototype.next = function(e) {
-  if(this.currentSlide == this.slideObj.length - 1) return false;
+
+  if(this.currentSlide === this.slideObjects.length - 1) return false;
+  
   this.currentSlide++;
   this.move(1);
+
+  return this;
+
 };
 
 SliderDiv.prototype.prev = function(e) {
-  if(this.currentSlide == 0) return false;
+
+  if(this.currentSlide === 0) return false;
+  
   this.currentSlide--;
   this.move(-1);
+
+  return this;
+
 };
 
-SliderDiv.prototype.autoPlay = function(_moveTime) {
+SliderDiv.prototype.autoPlay = function( _moveTime ) {
+  
   this.autoMoveTime = _moveTime || this.autoMoveTime;
 
   var that = this;
-  this.autoTimeout = setInterval(function() { that.play(); }, this.autoMoveTime);
+  
+  this.autoTimeout = setInterval(function() {
+
+    that.play(); 
+
+  }, this.autoMoveTime);
+
   return this;
 };
 
 SliderDiv.prototype.play = function() {
-  if(this.currentSlide == this.countSlides - 1)
+
+  if(this.currentSlide === this.countSlides - 1)
     this.moveTo(0);
   else
     this.next();
-};
 
-SliderDiv.prototype.stopPlay = function() {
-  clearTimeout(this.autoTimeout);
   return this;
 };
 
-SliderDiv.prototype.move = function(_direction, _distance) {
+SliderDiv.prototype.stopPlay = function() {
+
+  clearTimeout(this.autoTimeout);
+  return this;
+
+};
+
+SliderDiv.prototype.move = function( _direction, _distance ) {
+
   this.handleButtonVisibility();
+
+  this.highLightItem();
 
   // Move
   var distance = _distance ? _distance : this.container.width();
@@ -91,21 +186,27 @@ SliderDiv.prototype.move = function(_direction, _distance) {
   // Animation
   var that = this;
   this.viewport.animate({
+
     left: to + distance
+
   }, this.MOVE_SPEED, function() {
+
     that.afterMove();
+
   });
 
   return this;
 };
 
-SliderDiv.prototype.moveTo = function(_index) {
+SliderDiv.prototype.moveTo = function( _index ) {
+
   if($("input, textarea").is(":focus")) return false;
+  
   if(_index < 0 || _index == this.currentSlide || _index > this.countSlides) return false;
 
   // Direction
-  var currentLeft = this.slideObj.eq(this.currentSlide).offset().left;
-  var nextLeft = this.slideObj.eq(_index).offset().left;
+  var currentLeft = this.slideObjects.eq(this.currentSlide).offset().left;
+  var nextLeft = this.slideObjects.eq(_index).offset().left;
   var direction = currentLeft > nextLeft ? -1 : 1;
 
   // Distance
@@ -119,29 +220,57 @@ SliderDiv.prototype.moveTo = function(_index) {
 };
 
 SliderDiv.prototype.afterMove = function() {
+
   return true;
+
+};
+
+SliderDiv.prototype.highLightItem = function() {
+
+  if( !this.itemUl.length ) return false;
+
+  var items = this.itemUl.find("li");
+
+  items.removeClass("active-item");
+
+  items.eq(this.currentSlide).addClass("active-item");
+
 };
 
 SliderDiv.prototype.handleButtonVisibility = function() {
+  
   this.nextButton.show();
   this.prevButton.show()
-  if(this.currentSlide == this.slideObj.length - 1)            
+  
+  if(this.currentSlide == this.slideObjects.length - 1)            
     this.nextButton.hide();
+  
   if(this.currentSlide == 0)            
     this.prevButton.hide();
+
 };
 
-SliderDiv.prototype.getViewportHeight = function(_index) {
-  var hiddenFields = this.slideObj.eq(_index).find(':hidden').show();
-  var newH = this.slideObj.eq(_index).height();
-  hiddenFields.hide()
+SliderDiv.prototype.getViewportHeight = function( _index ) {
+
+  var hiddenFields = this.slideObjects.eq(_index).find(':hidden').show();
+  var newH = this.slideObjects.eq(_index).height();
+  
+  hiddenFields.hide();
+  
   return newH;
+
 };
 
 SliderDiv.prototype.keyEvent = function(e) {
-  if(!this.HAS_KEY_EVENTS) return false;  
+
+  if( !this.HAS_KEY_EVENTS ) return false;
+  
   switch(e.keyCode) {
-    case(39): this.next();break;
-    case(37): this.prev();break;
+
+    case(39): this.next(); break;
+
+    case(37): this.prev(); break;
+
   }
+
 };
